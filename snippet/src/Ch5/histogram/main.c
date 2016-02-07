c6488963ef0316b82c374b06196726033d540848
nhanloukiala <tantrokiniem@gmail.com>
2016-02-08 00:54:08 +0200

    test kernel

--- snippet/src/Ch5/histogram/main.c
+++ snippet/src/Ch5/histogram/main.c
@@ -166,7 +166,7 @@ int main(int argc, char** argv) {
 	    }
         /* Build OpenCL program object and dump the error message, if any */
         char *program_log;
-        const char options[] = "-cl-std=CL1.2";
+        const char options[] = "";
         size_t log_size;
 
         error = clBuildProgram(program, 1, &device, options, NULL, NULL);
